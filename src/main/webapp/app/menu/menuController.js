1dd2838bde6c25a8519cd3efc7decb0d50bcc7ba
Liao <qiang.liao@accenture.com>
2017-09-27 11:27:25 +0800

    angularJsdDemo

---
+++ src/main/webapp/app/menu/menuController.js
@@ -0,0 +1,20 @@
+/*App.controller('menuController', ['$scope', '$http', function ($scope, $http) {
+		 
+			alert("22222");
+ 
+		}]);*/
+		
+/*
+ *html： app/seat/ProjectSeat.html
+ *LQ
+ *7/11/2017
+ */
+var menuControllers = angular.module('menuControllers', []);/*
+menuControllers.service('projectSeatService', [ '$http', '$q', function($http, $q) {
+    return {
+    };
+} ]);*/
+menuControllers.controller('menuController', function($scope, $rootScope, $state, projectSeatService,userSessionService) {
+	
+	alert(1111)
+});
